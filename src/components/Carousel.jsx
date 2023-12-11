7d29fc27c4f9585f62668dded6e83df9c6567694
AlecCFrost <siva.ganesh.c18@gmaol.com>
2023-12-11 12:19:06 +0530

    Updated

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,6 +8,31 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+    const [currentImageIndex, setCurrentImage] = useState(0);
+
+    const handelNext = () => {
+        setCurrentImage((prevIndex) => (prevIndex + 1) % images.length);
+    }
+
+    const handelPrev = () => {
+        setCurrentImage((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    }
+
+    const currentImage = images[currentImageIndex];
+
+    return (
+        <div className="Container">
+            <ArrowBackIosIcon onClick={handelPrev} className="arrow-icon" />
+            <div className="image-cont">
+                <img src={currentImage.img} alt={currentImage.title} className="carousel-img" />
+                <div className="sub-cont">
+                    <h2>{currentImage.title}</h2>
+                    <p>{currentImage.subtitle}</p>
+                </div>
+            </div>
+            <ArrowForwardIosIcon onClick={handelNext} className="arrow-icon" />
+        </div>
+    )
 }
 
 export default Carousel;
